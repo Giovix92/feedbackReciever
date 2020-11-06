36cf49e08ab9c03371a5f25a23552d78e799397b
Sourav Gain <souravgain605@gmail.com>
2020-11-07 00:03:09 +0530

    allow content-type in post req

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const log = (req, res, next)=>{
 
 const allowCrossOrigin = (req, res, next)=>{
     res.header('Access-Control-Allow-Origin', "*")
+    res.header('Access-Control-Allow-Headers','Content-Type')
     next();
 }
 app.use(log)
